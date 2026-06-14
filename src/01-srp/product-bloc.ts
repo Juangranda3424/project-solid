e4735b27e14776057a96f0b07d76126020b22162
Juangranda3424 <juan.granda3424@gmail.com>
2026-06-13 20:16:37 -0500

    refactor(srp): separar lógica de productos y servicio de notificaciones

--- src/01-srp/product-bloc.ts
+++ src/01-srp/product-bloc.ts
@@ -1,39 +1,61 @@
-
-/**
- * VIOLACIÓN AL PRINCIPIO DE RESPONSABILIDAD ÚNICA (SRP)
- * 
- * Este archivo muestra una clase "Dios" o un componente que hace demasiadas cosas.
- * En el contexto de la Reserva Ecológica, el ProductBloc gestiona el inventario de la tienda
- * de souvenirs y, al mismo tiempo, se encarga de las notificaciones por correo.
- */
-
+// Interfaz para producto
 interface Product {
     id: number;
     name: string;
 }
 
-export class ProductBloc {
+// Interfaz para email
+interface Email {
+    email: string;
+    message: string;
+}
+
+// Reposabilidad Unica: persistencia de Datos
+class ProductRepository {
 
     private products: Product[] = [];
 
-    // Responsabilidad 1: Carga de productos (Lógica de Negocio/Persistencia)
-    loadProduct(id: number) {
+    // Carga de productos
+    load(id: number) {
         console.log(`Cargando producto con ID: ${id} desde el inventario del parque...`);
         // Simulación de carga
         return this.products.find(p => p.id === id);
     }
 
-    // Responsabilidad 2: Guardado de productos (Lógica de Persistencia)
-    saveProduct(product: Product) {
+    // Guardado de productos 
+    save(product: Product) {
         console.log(`Guardando el producto ${product.name} en la base de datos de la reserva...`);
         this.products.push(product);
     }
 
-    // Responsabilidad 3: Envío de notificaciones (Servicio de Infraestructura)
-    // ESTA ES LA VIOLACIÓN: El Bloc no debería saber CÓMO enviar correos electrónicos.
-    notifyCustomer(email: string, message: string) {
-        console.log(`[Mailer] Enviando correo a ${email}: ${message}`);
-        // Lógica directa de envío de correo acoplada aquí
+}
+
+// Reposabilidad Unica: envio de correos
+export class EmailService {
+    public send(data: Email){
+        console.log(`Enviando email a ${data.email}: ${data.message}`);
+    }
+}
+
+
+
+export class ProductBloc {
+    // Recibimos las dependencias necesarias
+    constructor(
+        private emailservice: EmailService,
+        private productrepository: ProductRepository
+    ){}
+    // Ahora solo coordina la carga usando el repositorio
+    loadProduct(id: number) {
+        return this.productrepository.load(id);
+    }
+    // Coordina el guardado usando el repositorio
+    saveProduct(product: Product) {
+        return this.productrepository.save(product);
+    }
+    // Delegamos la responsabilidad por completo al servicio de correo asi como su envio de informacion
+    notifyCustomer(data: any) {
+        return this.emailservice.send(data);
     }
 
 }
